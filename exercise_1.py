9aca976559d7fb10d6a18e93862e236e07f409c6
GabrielDully <gabrieldully@usp.br>
2024-02-17 17:45:08 -0300

    exercise_1.py
    Addition of the resolution of 'exercise_1.py'

--- exercise_1.py
+++ exercise_1.py
@@ -6,3 +6,12 @@ Display the first few rows of the DataFrame using the head() method.
 Use the info() method to display information about the DataFrame, such as the column names, data types, and non-null counts.
 '''
 
+import pandas as pd
+
+def read_and_summarize_csv_data():
+    
+    df = pd.read_csv('example.csv')
+
+    print(df.head())
+
+    df.info()
