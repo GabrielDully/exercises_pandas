1503c2b655a62cca19ac62954873b930eaeffdd8
GabrielDully <gabrieldully@usp.br>
2024-02-17 17:46:09 -0300

    exercise_2.py
    Addition of the resolution of 'exercise_2.py'

--- exercise_2.py
+++ exercise_2.py
@@ -4,3 +4,14 @@ Data Selection and Filtering:
 Select a single column from the DataFrame using square brackets or dot notation.
 Filter the DataFrame to include only rows that meet a specific condition, such as values greater than a certain threshold.
 '''
+
+import pandas as pd
+
+def data_selection_and_filtering():
+    
+    df = pd.read_csv('example.csv')
+
+    print(df['Index'])
+    print(df.Website)
+
+    print(df[df['Index'] > 95])
